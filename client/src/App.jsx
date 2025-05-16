5be053484127e227147070d057cb9c73de323ff1
abhishekyadav <yadavabhishek2723@gmail.com>
2025-05-17 02:04:38 +0530

    removed error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
-import Navbar from "./components/layouts/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
